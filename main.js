1a3b979442731d5c16f3c765719002a1b022ff94
byAlifer <163350886+byAlifer@users.noreply.github.com>
2024-05-07 09:32:09 -0300

    Alpha 1.1
    Menu adjusts

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const burguerMenu = document.querySelector('#burguer_menu')
+const exitMenu = document.querySelector('#exit_menu')
+const menuModal = document.querySelector('#menu_modal')
 
+burguerMenu.addEventListener('click', openModal)
+exitMenu.addEventListener('click', closeModal)
+
+function openModal() {
+    burguerMenu.style.display = 'none'
+    exitMenu.style.display = 'block'
+    menuModal.style.display = 'flex'
+} 
+
+function closeModal() {
+    exitMenu.style.display = 'none'
+    burguerMenu.style.display = 'block'
+    menuModal.style.display = 'none'
+}

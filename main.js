611a1bcc0c8cd3a8912f9109330e02c6a8051896
byAlifer <163350886+byAlifer@users.noreply.github.com>
2024-05-07 10:01:44 -0300

    Alpha 1.1.2
    Menu fixed

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+//Elements
 const burguerMenu = document.querySelector('#burguer_menu')
 const exitMenu = document.querySelector('#exit_menu')
 const menuModal = document.querySelector('#menu_modal')
 
+//events
 burguerMenu.addEventListener('click', openModal)
 exitMenu.addEventListener('click', closeModal)
 
+//functions
 function openModal() {
     burguerMenu.style.display = 'none'
     exitMenu.style.display = 'block'
@@ -16,3 +19,18 @@ function closeModal() {
     burguerMenu.style.display = 'block'
     menuModal.style.display = 'none'
 }
+
+//Fix Screen - menu (Mobile Edition)
+function fixScreen() {
+    let wScreen = window.innerWidth
+
+    if(wScreen >= 781) {
+        exitMenu.style.display = 'none'
+        burguerMenu.style.display = 'none'
+        menuModal.style.display = 'none'
+    } else if(wScreen <= 780 && exitMenu.style.display == 'none') {
+        burguerMenu.style.display = 'block'  
+    }
+}
+
+window.addEventListener('resize', fixScreen)
